b798ac87203d54baf2b11e24b523cf603c27978b
Lalman <lalmanchaudhary2@gmail.com>
2026-02-20 14:16:21 +0530

    fix: suppress hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
         <ThemeProvider>
           <QueryProvider>{children}</QueryProvider>
